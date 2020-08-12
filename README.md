7edd79227ee23c5d1d32d241e230eb7c2e995e14
Mike Koro <mikekoro86@gmail>
2020-08-11 21:11:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Micro Frontend #2
+
+Go to [https://github.com/mikekoro/mother-octopus](https://github.com/mikekoro/mother-octopus) to install the main wrapper.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{border:'1px solid green'}}>
+      <h3>Front end 2</h3>
+    </div>
+  );
+}
+
+export default App;
